dc492a8878f65acce04498d2cb88299e87292cf4
Andrei Dobrete <andy3153@protonmail.com>
2021-04-15 14:34:24 +0300

    Initial commit

---
+++ Functions/robotMovement.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.Functions;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+public class robotMovement {
+
+    public static void move(DcMotor FL, DcMotor FR, float speed) {//, DcMotor BL, DcMotor BR, float speed) {
+        FL.setPower(-speed);
+        FR.setPower(speed);
+        //BL.setPower(-speed);
+        //BR.setPower(speed);
+    }
+
+    public static void strafe(DcMotor FL, DcMotor FR, float speed) {//, DcMotor BL, DcMotor BR, float speed) {
+        FL.setPower(speed);
+        FR.setPower(speed);
+        //BL.setPower(-speed);
+        //BR.setPower(-speed);
+    }
+
+    public static void turn(DcMotor FL, DcMotor FR, float speed) {//, DcMotor BL, DcMotor BR, float speed) {
+        FL.setPower(speed);
+        FR.setPower(speed);
+        //BL.setPower(speed);
+        //BR.setPower(speed);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## BroBotsFTC_2020-2021
+
+This is the code the team RO142 BroBots uses for the Ultimate Goal season 2020-2021.
+
+In order for all this to work, you need to clone the [FTC Libs](https://github.com/FIRST-Tech-Challenge/FtcRobotController) first, then clone this repo inside of the folder TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ in that.
